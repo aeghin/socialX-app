618c88c234fc6bb6e9ec7a3452da6f4e8acc282d
Edward Castaneda <aeghin7@gmail.com>
2023-05-22 13:41:47 -0500

    added redux toolkit logic

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -28,6 +28,19 @@ export const authSlice = createSlice({
             } else {
                 console.log("user friends non-existent :(")
             };
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
         }
     }
 });
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
