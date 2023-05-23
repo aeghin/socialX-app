1161bb83ceb14f68897b660e23bfa7ef2d1b47e0
Edward Castaneda <aeghin7@gmail.com>
2023-05-23 11:54:31 -0500

    fixed index redux issues, added Flex abilities and MUI.

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -19,28 +19,29 @@ export const authSlice = createSlice({
             state.token = action.payload.token;
         },
         setLogout: (state) => {
-            state.user = null,
-                state.token = null
+            state.user = null;
+            state.token = null;
         },
         setFriends: (state, action) => {
             if (state.user) {
                 state.user.friends = action.payload.friends;
             } else {
-                console.log("user friends non-existent :(")
-            };
+                console.error("user friends non-existent :(");
+            }
         },
         setPosts: (state, action) => {
             state.posts = action.payload.posts;
         },
         setPost: (state, action) => {
             const updatedPosts = state.posts.map((post) => {
-                if (post._id === action.payload.post_id) return action.payload.post;
+                if (post._id === action.payload.post._id) return action.payload.post;
                 return post;
             });
             state.posts = updatedPosts;
-        }
-    }
+        },
+    },
 });
 
-export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+    authSlice.actions;
 export default authSlice.reducer;
