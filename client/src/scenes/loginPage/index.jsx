13e287da336620080d5fb0f45c38789daa9aae4c
Edward Castaneda <aeghin7@gmail.com>
2023-05-25 11:48:16 -0500

    finalized navbar logic

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,33 @@
+import {
+    Box,
+    Typography,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+
+
+
 const LoginPage = () => {
-    return (<div>Login page</div>)
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(mind-width: 1000px)");
+
+
+    return <Box>
+        <Typography
+            fontWeight="bold"
+            fontSize="clamp(1rem, 2rem, 2.25rem)"
+            color="primary"
+            onClick={() => navigate("/home")}
+            sx={{
+                "&:hover": {
+                    color: primaryLight,
+                    cursor: "pointer",
+                },
+            }}
+        >
+            SocialX
+        </Typography>
+    </Box>
 };
 
 export default LoginPage;
