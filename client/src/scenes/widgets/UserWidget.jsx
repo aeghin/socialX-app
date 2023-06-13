25a489fbc85fa3a303304d49fd3ee6271b3439a5
Edward Castaneda <aeghin7@gmail.com>
2023-06-12 22:55:09 -0500

    added rows for user widgets

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -59,6 +59,70 @@ const UserWidget = ({ userId, picturePath }) => {
                 pb="1.1rem"
                 onClick={() => navigate(`/profile/${userId}`)}
             >
+                <FlexBetween gap="1rem">
+                    <UserImage image={picturePath} />
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            color={dark}
+                            fontWeight="500"
+                            sx={{
+                                "&:hover": {
+                                    color: palette.primary.light,
+                                    cursor: "pointer"
+                                }
+                            }}
+                        >
+                            {firstName} {lastName}
+                        </Typography>
+                        <Typography color={medium}>{friends.length} friends</Typography>
+                    </Box>
+                    <ManageAccountsOutlined />
+                </FlexBetween>
+                <Divider />
+
+
+                {/* SECOND ROW */}
+                <Box p="1rem 0">
+                    <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                        <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+                        <Typography color={medium}>{location}</Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" gap="1rem">
+                        <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+                        <Typography color={medium}>{occupation}</Typography>
+                    </Box>
+                </Box>
+                {/* THIRD ROW   */}
+
+                <Box p="1rem 0">
+                    <FlexBetween mb="0.5rem">
+                        <Typography color={medium}>Who's viewed your profile</Typography>
+                        <Typography color={main} fontWeight="500">{viewedProfile}</Typography>
+                    </FlexBetween>
+                    <FlexBetween>
+                        <Typography color={medium}>Impressions of your post</Typography>
+                        <Typography color={main} fontWeight="500">{impressions}</Typography>
+                    </FlexBetween>
+                </Box>
+
+                {/* FOURTH ROW  */}
+
+                <Box p="1rem 0">
+                    <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+                        Social Profiles
+                    </Typography>
+                    <FlexBetween gap="1rem" mb="0.5rem">
+                        <FlexBetween gap="1rem">
+                            <img src="../assets/twitter.png" alt="twitter logo" />
+                            <Box>
+                                <Typography color={main} fontWeight="500">
+                                    SocialX
+                                </Typography>
+                            </Box>
+                        </FlexBetween>
+                    </FlexBetween>
+                </Box>
 
             </FlexBetween>
         </WidgetWrapper>
