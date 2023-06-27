6cc76ae0fab0e0acc32a10a409b3fb07275aa25c
Edward Castaneda <aeghin7@gmail.com>
2023-06-26 19:27:22 -0500

    added widget functionality

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -52,17 +52,17 @@ const PostsWidget = ({ userId, isProfile = false }) => {
                     likes,
                     comments,
                 }) => (
-                    <PostWidget 
-                    key={_id}
-                    postId={_id}
-                    postUserId={userId}
-                    name={`${firstName} ${lastName}`}
-                    description={description}
-                    location={location}
-                    picturePath={picturePath}
-                    userPicturePath={userPicturePath}
-                    likes={likes}
-                    comments={comments}
+                    <PostWidget
+                        key={_id}
+                        postId={_id}
+                        postUserId={userId}
+                        name={`${firstName} ${lastName}`}
+                        description={description}
+                        location={location}
+                        picturePath={picturePath}
+                        userPicturePath={userPicturePath}
+                        likes={likes}
+                        comments={comments}
                     />
                 )
 
