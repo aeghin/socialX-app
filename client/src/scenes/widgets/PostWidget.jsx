8c11f2b60581aaf6141e600d029478a5637881d5
Edward Castaneda <aeghin7@gmail.com>
2023-06-27 22:01:57 -0500

    request for Likes function added

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -12,6 +12,42 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "state";
 
-const PostWidget = ({ }) => {
+const PostWidget = ({
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments,
+}) => {
+    const [isComment, setIsComment] = useState(false);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+    const isLiked = Boolean(likes[loggedInUserId]);
+    const likeCount = Object.keys(likes).length;
 
+    const { palette } = useTheme();
+    const primary = palette.primary.main;
+    const main = palette.neutral.main;
+
+
+    const patchLike = async () => {
+        const response = await fetch(`http//localhost:3001/posts/${postId}/like`,
+            {
+                method: "PATCH",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ userId: loggedInUserId }),
+            });
+        const updatedPost = await response.json();
+        dispatch(setPost({ post: updatedPost }))
+    };
 };
+
+export default PostWidget;
