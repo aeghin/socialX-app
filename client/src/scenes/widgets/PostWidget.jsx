132032c98fe1afdd0275502764f5bdd2f0095709
Edward Castaneda <aeghin7@gmail.com>
2023-06-28 21:42:30 -0500

    posts fully functional along with likes functionality.

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,10 +1,10 @@
 import {
     ChatBubbleOutlineOutlined,
-    FavoriteBorderOutlines,
+    FavoriteBorderOutlined,
     FavoriteOutlined,
     ShareOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typograph, useTheme } from "@mui/material";
+import { Box, Divider, IconButton, Typograph, Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
 import WidgetWrapper from "components/WidgetWrapper";
@@ -23,7 +23,7 @@ const PostWidget = ({
     likes,
     comments,
 }) => {
-    const [isComment, setIsComment] = useState(false);
+    const [isComments, setIsComments] = useState(false);
     const dispatch = useDispatch();
     const token = useSelector((state) => state.token);
     const loggedInUserId = useSelector((state) => state.user._id);
@@ -48,6 +48,66 @@ const PostWidget = ({
         const updatedPost = await response.json();
         dispatch(setPost({ post: updatedPost }))
     };
+
+    return (
+        <WidgetWrapper m="2rem 0">
+            <Friend
+                friendId={postUserId}
+                name={name}
+                subtitle={location}
+                userPicturePath={userPicturePath}
+            />
+            <Typography color={main} sx={{ mt: "1rem" }}>
+                {description}
+            </Typography>
+            {picturePath && (
+                <img
+                    width="100%"
+                    height="auto"
+                    alt="post"
+                    style={{ borderRadius: "0.5rem", marginTop: "0.75rem" }}
+                    src={`http//localhost:3001/assets/${picturePath}`}
+                />
+            )}
+            <FlexBetween mt="0.25rem">
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={patchLike}>
+                            {isLiked ? (
+                                <FavoriteOutlined sx={{ color: primary }} />
+                            ) : (
+                                <FavoriteBorderOutlined />
+                            )}
+                        </IconButton>
+                        <Typography>{likeCount}</Typography>
+                    </FlexBetween>
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={() => setIsComments(!isComments)}>
+                            <ChatBubbleOutlineOutlined />
+                        </IconButton>
+                        <Typography>{comments.length}</Typography>
+                    </FlexBetween>
+                </FlexBetween>
+                <IconButton>
+                    <ShareOutlined />
+                </IconButton>
+            </FlexBetween>
+            {isComments && (
+                <Box
+                    mt="0.5rem"
+                >
+                    {comments.map((comment, i) => (
+                        <Box key={`${name}-${i}`}>
+                            <Divider />
+                            <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                                {comment}
+                            </Typography>
+                        </Box>
+                    ))}
+                </Box>
+            )}
+        </WidgetWrapper>
+    )
 };
 
 export default PostWidget;
