f0f0fa0aee69f92e34826d727679514a808b1d28
Edward Castaneda <aeghin7@gmail.com>
2023-07-27 22:49:08 -0500

    added comments to better understand user routes functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
     try {
-
+            // this id will be mongoose _id 
         const { id } = req.params;
         const user = await User.findById(id);
         res.status(200).json(user);
@@ -17,17 +17,19 @@ export const getUser = async (req, res) => {
 export const getUserFriends = async (req, res) => {
 
     try {
+
+         // this id will be mongoose _id 
         const { id } = req.params;
         const user = await User.findById(id);
-
+        // mapping over user.friends array and getting all their info with friends id(s)
         const friends = await Promise.all(
             user.friends.map(id => User.findById(id))
         );
-
+            // destructuring friends object with these properties to use in the front-end
         const formattedFriends = friends.map(({ _id, firstName, lastName, occupation, location, picturePath }) => {
             return { _id, firstName, lastName, occupation, location, picturePath }
         });
-
+            //sending this object to the front-end
         res.status(200).json(formattedFriends)
     } catch (err) {
         res.status(404).json({ message: err.message })
@@ -41,11 +43,12 @@ export const addRemoveFriend = async (req, res) => {
         const { id, friendId } = req.params;
         const user = await User.findById(id);
         const friend = await User.findById(friendId);
-
+        // removing friends from friends arr on both user and friend.
         if (user.friends.includes(friendId)) {
             user.friends = user.friends.filter((id) => id !== friendId);
             friend.friends = friend.friends.filter((id) => id !== id);
         } else {
+            // otherwise here we are pushing the friends so they are added as friends
             user.friends.push(friendId);
             friend.friends.push(id);
         };
