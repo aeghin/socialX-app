9134b19dfa9f2d0718b6202203b4dfa155f7b095
Edward Castaneda <aeghin7@gmail.com>
2023-08-15 19:23:59 -0500

    re-worded social motto

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -20,6 +20,8 @@ import {
     Menu,
     Close,
 } from "@mui/icons-material";
+
+
 import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "state";
 import { useNavigate } from "react-router-dom";
